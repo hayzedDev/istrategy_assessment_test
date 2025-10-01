96a0a4d941112ac8b808537053a27443a29cc3b1
Azeez Abdulsalam <azeezade04@gmail.com>
2025-10-01 15:42:01 +0100

    Adds dependency install step to process startup
    Ensures all required dependencies are installed before running
    migrations, improving reliability during deployment and execution.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'payment-processor',
       script:
-        'yarn build &&  yarn  migration:run && yarn copy-seed-data && yarn start', // Path to your compiled main file
+        'yarn build && yarn install &&  yarn  migration:run && yarn copy-seed-data && yarn start', // Path to your compiled main file
       instances: 'max', // Or a number like 2
       autorestart: true,
       watch: false,
